4e8509218fb5eb035ad736ae6839474f5dbb6458
pradyoth98 <69283359+pradyoth98@users.noreply.github.com>
2020-09-18 00:50:52 +0530

    First two shell scripts
    run.sh checks if a given webpage is capable of establishing a connection or not. 
    
    test.sh checks if any change has been detected in a webpage since starting the script.
    
    Both scripts work with BASH. And yes, both run correctly by default on Debian based systems. 
    
    The names of these scripts could've been better. But eh, that's alright

---
+++ run.sh
@@ -0,0 +1,30 @@
+val=true
+wget --no-check-certificate results.vtu.ac.in >/dev/null 2>&1
+hash=$(sha1sum index.html)
+rm index.html
+while $val;
+do
+wget --no-check-certificate results.vtu.ac.in >/dev/null 2>&1
+nhash=$(sha1sum index.html)
+rm index.html
+
+if [ "$hash" = "$nhash" ]
+then
+	echo No change detected
+	echo Last updated at $(date | awk '{print $4}')
+else
+	echo Change detected
+	echo Last updated at $(date | awk '{print $4}')
+	val=false
+fi
+
+sleep 60s
+tput cuu1;tput el;
+tput cuu1;tput el;
+done
+
+
+while true;
+do
+	echo -en "\007"	
+done
---
+++ test.sh
@@ -0,0 +1,27 @@
+val=true
+while $val;
+do
+stat=$(nc -z results.vtu.ac.in 80; echo $?)
+if [ $stat -eq 0 ]
+then
+	echo Server still running
+	echo Last updated at $(date | awk '{print $4}')
+else
+	echo Server down
+	echo Last updated at $(date | awk '{print $4}')
+	val=false
+fi
+
+
+
+
+sleep 60s
+tput cuu1;tput el;
+tput cuu1;tput el;
+done
+
+
+while true;
+do
+	echo -en "\007"	
+done
